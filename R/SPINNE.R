5af9532737fbc3572c6b7cf780fd3b12c2cf064c
dsnavega <dsnavega@gmail.com>
2019-03-06 13:05:13 +0000

    [+] Release v0.1.0 (FSI)

--- R/SPINNE.R
+++ R/SPINNE.R
@@ -6,7 +6,7 @@
 #' 
 SPINNE <- function() {
   
-  spinne_dir <- system.file("SPINNE", package = "SPINNE", mustWork = T)
+  spinne_dir <- system.file("app", package = "SPINNE", mustWork = T)
   spinne_app <- shiny::shinyAppDir(appDir = spinne_dir)
   shiny::runApp(
     appDir = spinne_app,
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,152 +1,18 @@
-source('global.R')
+library(shiny)
+library(grnnet)
+library(corrplot)
+library(ggplot2)
+
+vertebrae.db <- readRDS(file = "data/dvb.rds")
+vertebrae.female <- vertebrae.db[vertebrae.db$SEX == 'Female', ]
+vertebrae.male <- vertebrae.db[vertebrae.db$SEX == 'Male', ]
+codenames <- colnames(vertebrae.db[ , -c(1:3)])
+outnames <- codenames
+
 shinyUI(
   fluidPage(
     navbarPage(
       'SPINNE', collapsible = T, theme = 'bootstrap.css',
-      tabPanel(
-        'About',
-        fluidPage(
-          fluidRow(
-            h1('Spine Proportion through Implementation of Neural Networks', align = 'center')
-          ),
-          br(),
-          fluidRow(
-            h3('Authors'),
-            column(12,
-                   p("Daniela Vilas Boas, Sofia N. Wasterlain, João D'Oliveira Coelho, David Navega, David Gonçalves" )
-            )
-          ),
-          fluidRow(
-            h3('Reference'),
-            column(12,
-                   p(align = 'justify', a(href = 'dvb_dissertation.pdf', target = '_blank', 'Daniela Vilas Boas, 2016.
-									Missing values: estimativa da altura de vértebras ausentes
-									ou mal preservadas para a aplicação do método anatómico na estimativa da estatura.'),
-                     'Dissertação de Mestrado em Evolução e Biologia Humanas, orientada pela 
-							Professora Doutora Sofia Wasterlain e pelo Doutor David Gonçalves e
-							apresentada ao Departamento de Ciências da Vida da Faculdade de Ciências
-							e Tecnologia da Universidade de Coimbra')
-                   
-            )
-          ),
-          fluidRow(
-            h3('App Development'),
-            column(6,
-                   a(href = 'http://jcoelho.com', target = '_blank', p("João d'Oliveira Coelho")),
-                   a(href = 'https://github.com/dsnavega', target = '_blank', p('David Navega'))
-            ),
-            column(6,
-                   a(href = 'http://osteomics.com', target = '_blank',
-                     img(src = 'http://osteomics.com/img/logo.png',
-                         align = 'right')
-                   )
-            )
-          )
-        )
-      ), # tabPanel('About')
-      tabPanel(
-        'Data Exploration',
-        fluidPage(
-          tabsetPanel(
-            id = 'tabsetExplore',
-            tabPanel(
-              'Density Plots',
-              fluidRow(
-                sidebarPanel(
-                  selectInput('dpvar', 'Variable', choices = codenames),
-                  hr(),
-                  checkboxInput('filter', 'Filter by Sex', FALSE),
-                  hr(),
-                  selectInput('kernel', 'Kernel: ',
-                              choices = c('gaussian', 'epanechnikov', 'rectangular', 'triangular', 'cosine')),
-                  sliderInput('bandwidth', 'Bandwidth:', 
-                              min = 0.05, max = 2, value = 1),
-                  sliderInput('alpha', 'Filling opacity:',
-                              min = 0.01, max = 1, value = 0.5)
-                ),
-                mainPanel(
-                  fluidRow(
-                    plotlyOutput('densPlot')
-                  ),
-                  fluidRow(
-                    hr(),
-                    textOutput('normTest')
-                  )
-                )
-              )
-            ),
-            tabPanel(
-              'Box Plots',
-              fluidRow(
-                sidebarPanel(
-                  selectInput('bpvar', 'Variable', choices = codenames),
-                  hr(),
-                  checkboxInput('truedata', 'Add data points', FALSE),
-                  hr(),
-                  strong('Lower Whisker'),
-                  p('Smallest observation greater than or equal to lower hinge - 1.5 * IQR'),
-                  strong('Lower Hinge'),
-                  p('25% quantile'),
-                  strong('Middle'),
-                  p('Median, 50% quantile'),
-                  strong('Diamond'),
-                  p('Arithmetic mean'),
-                  strong('Upper Hinge'),
-                  p('75% quantile'),
-                  strong('Upper Whisker'),
-                  p('Largest observation less than or equal to upper hinge + 1.5 * IQR')
-                ),
-                mainPanel(
-                  plotlyOutput('boxPlot')
-                )
-              )
-            ),
-            tabPanel(
-              'Scatter Plots',
-              fluidRow(
-                sidebarPanel(
-                  selectInput('xvar', 'X-axis variable', codenames, selected = 'C2'),
-                  selectInput('yvar', 'Y-axis variable', codenames, selected = 'S1'),
-                  hr(),
-                  checkboxInput('filter2', 'Filter by Sex', FALSE),
-                  hr(),
-                  sliderInput('spanner', label = 'Smoothing:', value = 0.6, min = 0.2, max = 1)
-                ),
-                mainPanel(
-                  plotlyOutput('scatterPlot')
-                )
-              ) # fluidRow [Plot]
-            ), # tabPanel 'Scatter Plots'
-            tabPanel(
-              'Correlation Matrix',
-              fluidRow(
-                sidebarPanel(
-                  selectInput('cor.filter', 'Filter dataset by sex:',
-                              c('all', 'males', 'females'), 'all'),
-                  selectInput('cor.method', 'Correlation Method',
-                              c('pearson', 'kendall', 'spearman'), 'pearson'),
-                  selectInput('upper.visuals', 'Visuals Upper',
-                              eval(formals(corrplot)$method), 'square'),
-                  selectInput('lower.visuals', 'Visuals Lower',
-                              eval(formals(corrplot)$method), 'square'),
-                  selectInput('orderVisuals', 'Reorder Algorithm',
-                              eval(formals(corrplot)$order)),
-                  conditionalPanel(
-                    condition = "input.orderVisuals == 'hclust'",
-                    selectInput("plotHclustMethod", "Hierarchical clustering method",
-                                eval(formals(corrplot)$hclust.method)),
-                    numericInput("plotHclustAddrect", "Number of Rectangles", 3, 0, NA)
-                  )
-                  
-                ),
-                mainPanel(
-                  plotOutput('corrPlot', height = 700)
-                )
-              ) # fluidRow [Correlations]
-            ) # tabPanel [Correlations]
-          ) # tabsetPanel [Explore]
-        ) # fluidPage [Explore]
-      ), # tabPanel [Explore]
       tabPanel(
         'Estimate Missing Values',
         fluidPage(
@@ -157,7 +23,7 @@ shinyUI(
                                selected = 'Unkown'),
                    strong('Input Variables'),
                    selectInput(
-                     'variable', '(predictive vertebrae types - usually vertebra adjacent to the ones missing)',
+                     'variable', '(predictive vertebrae types - usually vertebrae adjacent to the ones missing)',
                      choices = c(codenames), multiple = TRUE,
                      selected = sample(codenames, size = 3)),
                    strong('Output Variables'),
@@ -200,6 +66,109 @@ shinyUI(
         )
       ),
       tabPanel(
+        'Data Exploration',
+        fluidPage(
+          tabsetPanel(
+            id = 'tabsetExplore',
+            tabPanel(
+              'Density Plots',
+              fluidRow(
+                sidebarPanel(
+                  selectInput('dpvar', 'Variable', choices = codenames),
+                  hr(),
+                  checkboxInput('filter', 'Filter by Sex', FALSE),
+                  hr(),
+                  selectInput('kernel', 'Kernel: ',
+                              choices = c('gaussian', 'epanechnikov', 'rectangular', 'triangular', 'cosine')),
+                  sliderInput('bandwidth', 'Bandwidth:', 
+                              min = 0.05, max = 2, value = 1),
+                  sliderInput('alpha', 'Filling opacity:',
+                              min = 0.01, max = 1, value = 0.5)
+                ),
+                mainPanel(
+                  fluidRow(
+                    plotOutput('densPlot')
+                  ),
+                  fluidRow(
+                    hr(),
+                    textOutput('normTest')
+                  )
+                )
+              )
+            ),
+            tabPanel(
+              'Box Plots',
+              fluidRow(
+                sidebarPanel(
+                  selectInput('bpvar', 'Variable', choices = codenames),
+                  hr(),
+                  checkboxInput('truedata', 'Add data points', FALSE),
+                  hr(),
+                  strong('Lower Whisker'),
+                  p('Smallest observation greater than or equal to lower hinge - 1.5 * IQR'),
+                  strong('Lower Hinge'),
+                  p('25% quantile'),
+                  strong('Middle'),
+                  p('Median, 50% quantile'),
+                  strong('Diamond'),
+                  p('Arithmetic mean'),
+                  strong('Upper Hinge'),
+                  p('75% quantile'),
+                  strong('Upper Whisker'),
+                  p('Largest observation less than or equal to upper hinge + 1.5 * IQR')
+                ),
+                mainPanel(
+                  plotOutput('boxPlot')
+                )
+              )
+            ),
+            tabPanel(
+              'Scatter Plots',
+              fluidRow(
+                sidebarPanel(
+                  selectInput('xvar', 'X-axis variable', codenames, selected = 'C2'),
+                  selectInput('yvar', 'Y-axis variable', codenames, selected = 'S1'),
+                  hr(),
+                  checkboxInput('filter2', 'Filter by Sex', FALSE),
+                  hr(),
+                  sliderInput('spanner', label = 'Smoothing:', value = 0.6, min = 0.2, max = 1)
+                ),
+                mainPanel(
+                  plotOutput('scatterPlot')
+                )
+              ) # fluidRow [Plot]
+            ), # tabPanel 'Scatter Plots'
+            tabPanel(
+              'Correlation Matrix',
+              fluidRow(
+                sidebarPanel(
+                  selectInput('cor.filter', 'Filter dataset by sex:',
+                              c('all', 'males', 'females'), 'all'),
+                  selectInput('cor.method', 'Correlation Method',
+                              c('pearson', 'kendall', 'spearman'), 'pearson'),
+                  selectInput('upper.visuals', 'Visuals Upper',
+                              eval(formals(corrplot)$method), 'number'),
+                  selectInput('lower.visuals', 'Visuals Lower',
+                              eval(formals(corrplot)$method), 'square'),
+                  selectInput('orderVisuals', 'Reorder Algorithm',
+                              eval(formals(corrplot)$order)),
+                  conditionalPanel(
+                    condition = "input.orderVisuals == 'hclust'",
+                    selectInput("plotHclustMethod", "Hierarchical clustering method",
+                                eval(formals(corrplot)$hclust.method)),
+                    numericInput("plotHclustAddrect", "Number of Rectangles", 3, 0, NA)
+                  )
+                  
+                ),
+                mainPanel(
+                  plotOutput('corrPlot', height = 700)
+                )
+              ) # fluidRow [Correlations]
+            ) # tabPanel [Correlations]
+          ) # tabsetPanel [Explore]
+        ) # fluidPage [Explore]
+      ), # tabPanel [Explore]
+      tabPanel(
         'Help',
         fluidPage(
           h3('Descriptions'),
@@ -241,6 +210,64 @@ shinyUI(
           p('(Spreading caliper)')
         )
       ),
+      tabPanel(
+        'About',
+        fluidPage(
+          fluidRow(
+            h1('Spine Proportion through Implementation of Neural Networks', align = 'center')
+          ),
+          br(),
+          fluidRow(
+            h3('Authors'),
+            column(12,
+                   p("Daniela Vilas Boas, Sofia N. Wasterlain, João d'Oliveira Coelho, David Navega David Gonçalves")
+            )
+          ),
+          fluidRow(
+            h3('References'),
+            column(12,
+                   p(align = 'justify', 'COMING SOON: Vilas Boas et al. 2019. Forensic Science International.'),
+                   column(12,
+                          p(align = 'justify', a(href = 'dvb_dissertation.pdf', target = '_blank', 'Daniela Vilas Boas, 2016.
+                                          Missing values: estimativa da altura de vértebras ausentes
+                                          ou mal preservadas para a aplicação do método anatómico na estimativa da estatura.'),
+                            'Dissertação de Mestrado em Evolução e Biologia Humanas, orientada pela 
+                     Professora Doutora Sofia Wasterlain e pelo Doutor David Gonçalves e
+                     apresentada ao Departamento de Ciências da Vida da Faculdade de Ciências
+                     e Tecnologia da Universidade de Coimbra')
+                          
+                   )
+            )
+          ),
+          fluidRow(
+            h3('Disclaimer'),
+            column(12,
+                   p("This", strong("decision support system"), "is freely provided as an aid for
+                   profiling skeletal material. The authors hold no responsibility
+                   for its ultimate use or misuse. While creators we try to ensure
+                   that the software is theoretically grounded and statically accurate,
+                     we provide no warranty and make no specific claims as to its performance
+                     or its appropriateness for use in any particular situation. Please read
+                     any research paper regarding this tool before applying it.
+                     Always keep in mind: you are the expert, this is just decision support system."
+                   )
+            )
+          ),
+          fluidRow(
+            h3('App Development'),
+            column(6,
+                   a(href = 'http://jcoelho.com', target = '_blank', p("João d'Oliveira Coelho")),
+                   a(href = 'https://github.com/dsnavega', target = '_blank', p('David Navega'))
+            ),
+            column(6,
+                   a(href = 'http://osteomics.com', target = '_blank',
+                     img(src = 'http://osteomics.com/img/logo.png',
+                         align = 'right')
+                   )
+            )
+          )
+        )
+      ), # tabPanel('About')
       tabPanel(
         title = HTML("<li><a href='http://osteomics.com' target='_blank'>Back to osteomics.com</a></li>")
       )
